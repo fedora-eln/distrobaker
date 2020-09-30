ac2f80e6948b4b34e0f52cf13f0d8b34ea2e64f0
Petr Šabata <contyk@redhat.com>
2020-09-30 14:44:05 +0200

    Merging a working branch
    Includes documentation extension, changes in plans, builder code,
    logging code, and merge options.
    
    Signed-off-by: Petr Šabata <contyk@redhat.com>

--- README.md
+++ README.md
@@ -7,6 +7,20 @@ automatic component builds in the downstream distribution.
 
 Initially written for Red Hat Enterprise Linux and CentOS Stream.
 
+## Current features
+
+* Configuration fetching and parser
+* Fedora messaging bus monitoring and its `buildsys.tag` messages
+* Git repository manipulation for SCM syncs (unauthenticated)
+
+## Planned
+
+* Reload configuration on the fly at certain intervals or with
+  each tag message (what's more feasible)
+* Tests with a local broker
+* A lot more error checking and recovery
+* Configurable merge message
+
 ## Implementation
 
 The tool fetches its configuration from a git repository
@@ -26,28 +40,7 @@ profile.
 
 Currently only Koji and Fedora Messaging are supported.
 
-## Current features
-
-* Configuration fetching and parser
-* Fedora messaging bus monitoring and its `buildsys.tag` messages
-* Git repository manipulation for SCM syncs (unauthenticated)
-
-## Planned
-
-* Authenticated SCM syncs
-* Authenticated component builds
-* Reload configuration on the fly at certain intervals or with
-  each tag message (what's more feasible)
-* REST API for status monitoring
-* Authenticated REST API for manual sync and build triggers
-* Logging
-* Tests with a local broker
-* A lot more error checking and recovery
-
 ## Configuration format
-
-This is not the final format yet and is subject to change as the
-tool develops.
 
 The tool expects the configuration file to be located in
 `${DISTROBAKERCONF}/distrobaker.yaml`.  A non-functional example
@@ -63,6 +56,7 @@ configuration:
     target: rhel-9.0.0-candidate
     profile: brew
     build: false
+    merge: false
 # Component configuration.  At least one component is required.
 # Source and destination properties are required and are appended
 # to the configuration properties of the same names.  Branch names
@@ -78,4 +72,117 @@ components:
         trigger: glibc-test-tag
         target: rhel-9.0.0-glibc-test-candidate
         build: true
+        merge: true
 ```
+
+### Flags
+
+This section explains each of the tags noted in the example above.
+
+#### `source`
+
+In the `configuration` section this holds the global dist-git prefix
+holding the sources and it's mandatory.  An example would be a local
+directory with bare repositories, or a remote dist-git instance, including
+the namespace.
+
+In the `components` section this holds the component's suffix that is
+appended to the global prefix, creating a path to the unique repository.
+It may optionally include the branch name, separated from the repository
+name with the pound symbols (`#`).  If the branch name is not provided,
+`master` is assumed.  Mandatory.
+
+#### `destination`
+
+Analogous to source, in the `configuration` section this holds the global
+prefix for all destination repositories and is mandatory.
+
+In the `components` section, this key holds the destination repository name
+which is appended to the global prefix, with the optional branch name
+at the end following the pound symbol (`#`).  If the branch is not provided,
+`master` is assumed.  Mandatory.
+
+#### `trigger`
+
+Defines the Koji tag name serving as a trigger during the build tagging event.
+This can be defined both globally in the `configuration` section for all
+components, or individually overridden in the `components` section.
+
+If not defined per component, the global default is used.  The default is
+mandatory, per component definitions are optional.
+
+DistroBaker watches for the `trigger` build tagging events on the source
+Koji instance via fedora-messaging configured in `FEDORA_MESSAGING_CONF`.
+
+#### `target`
+
+Defines the destination Koji build target, a Koji-defined pair of buildroot
+and destination tags in the destination Koji instance.  This can be defined
+both globally in the `configuration` section for all components, or individually
+overrides in the `components` section.
+
+If not defined per component, the global default is used.  The default is
+mandatory, per component definitions are optional.
+
+Destination Koji instance is determined by the profile flag, see below.
+
+#### `profile`
+
+Defines the rpkg profile used, configuring access to the destinations Koji
+instance.  The DistroBaker instance must have access credentials to access
+the target instance, namely a valid Kerberos ticket.
+
+The flag can be configured globally in the `configuration` section and
+individually overrides in the `components` section.
+
+The global default is mandatory, the per component configuration optional.
+If per component value is not defined, the global is used.
+
+#### `build`
+
+A boolean value controlling whether component builds should also be submitted.
+Like most flags, it can be defined globally in the `configuration` section and
+individually overriden in the `components` section.
+
+The global default is mandatory, the per component configuration optional.  If
+per component value is not defined, the global is used.
+
+If true, DistroBaker will submit builds to the destination Koji instance as per
+`profile` and `target` flags.
+
+If false, DistroBaker will only sync dist-git content.
+
+#### `merge`
+
+A boolean value controlling whether DistroBaker should merge changes with a merge
+commit in cases where clean fast forward merges (or pulls) are not possible.
+Like most flags, it can be defined globally in the `configuration` section and
+individually override in the `components` section.
+
+The global default is mandatory, the per component configuration optional.  If
+per component value is not defined, the global is used.
+
+If true, DistroBaker will merge source branches into destination branches using
+the `-X theirs` strategy and create a merge commit using `DISTROBAKERUSER` and
+`DISTROBAKEREMAIL`.  The commit message itself is currently hardcoded.
+
+If false, DistroBaker will report a warning and continue.
+
+Note that pushing merge commits will prevent future pulls and all syncs from that
+point will require a new merge commit.  Also note the potential of destroying
+downstream-only changes with this approach.  The recommended setting is `false`.
+
+## Scripts
+
+The `scripts` directory contains various scripts extending the code DistroBaker
+functionality.  Currently only one is provided:
+
+### `initialsync.py`
+
+The `initialsync.py` script performs the initial sync based on the configuration
+without listening to messages and triggering syncs and builds via tagging events.
+
+Its purpose is to prepare the target distribution in one go and should generally
+only be used once, with DistroBaker later keeping it up to date.
+
+It uses the DistroBaker module and `DISTROBAKER` variables have to be set.
--- distrobaker/__init__.py
+++ distrobaker/__init__.py
@@ -21,9 +21,6 @@
 #  * Automatically submits package builds from the synced branch
 #    in the configured build system.
 #
-#  * Provides REST API for monitoring the tasks and manual sync
-#    and build triggers for configured components.
-#
 
 import fedora_messaging
 from fedora_messaging import api as msgapi
@@ -35,6 +32,7 @@ import os
 import shutil
 import tempfile
 import yaml
+
 
 # Global configuration dict
 # Holds the generic configuration as well as the components.
@@ -68,6 +66,7 @@ def load_config():
     """
     global conf
     global status
+    logging.info('Loading configuration from ' + conf['configuration'])
     try:
         shutil.rmtree('conf')
     except FileNotFoundError:
@@ -81,7 +80,7 @@ def load_config():
         status['status'] = 'error - no configuration'
         return
     try:
-        for prop in ('source', 'destination', 'trigger', 'target', 'profile', 'build'):
+        for prop in ('source', 'destination', 'trigger', 'target', 'profile', 'build', 'merge'):
             conf[prop] = yamlconf['configuration'][prop]
         conf['components'] = {}
         for component in yamlconf['components'].keys():
@@ -91,7 +90,7 @@ def load_config():
                 'destination':
                     conf['destination'] + yamlconf['components'][component]['destination'],
             }
-            for prop in ('trigger', 'target', 'build'):
+            for prop in ('trigger', 'target', 'profile', 'build', 'merge'):
                 if prop in yamlconf['components'][component].keys():
                     compdata[prop] = yamlconf['components'][component][prop]
                 else:
@@ -113,29 +112,40 @@ def merge_component(component):
     srclink = split_branch(conf['components'][component]['source'])
     dstlink = split_branch(conf['components'][component]['destination'])
     try:
+        logging.debug('Fetching repositories...')
         repo = git.Repo.clone_from(dstlink['url'], repodir, branch=dstlink['branch'])
-        srcremote = repo.create_remote('src', conf['components'][component]['source'], branch=srclink['branch'])
+        srcremote = repo.create_remote('source', srclink['url'])
         srcremote.fetch()
         try:
+            logging.info('Attempting a fast forward merge...')
             repo.remotes.source.pull(srclink['branch'])
+            logging.info('Successfully pulled.')
         except git.exc.GitCommandError:
-            # TODO: Rewrite this to use the API
-            actor = git.Actor(conf['gituser'], conf['gitemail'])
-            repo.git.reset('--hard')
-            repo.git.merge('--no-commit', '-s', 'recursive', '-X', 'theirs', 'source/' + srclink['branch'])
-            commitmessage = """Automatic DistroBaker synchronization
+            if not conf['components'][component]['merge']:
+                logging.warning('Failed to do a fast forward merge but configured not to do a merge commit, skpping.')
+            else:
+                # TODO: Rewrite this to use the API
+                logging.info('Failed to do a fast forward merge.  Merging with a commit.')
+                actor = git.Actor(conf['gituser'], conf['gitemail'])
+                repo.git.reset('--hard')
+                repo.git.merge('--no-commit', '-s', 'recursive', '-X', 'theirs', 'source/' + srclink['branch'])
+                commitmessage = """Automatic DistroBaker synchronization
 
-            To opt out from automatic synchronization from monitored upstream,
-            consult the distribution documentation or reach out to the EXD/RCM team.
-            """
-            commitmessage += '\nTriggered by ' + component + ' appearing in ' + conf['components'][component]['trigger']
-            commitmessage += '\nSource: ' + conf['components'][component]['source']
-            repo.index.commit(commitmessage, author=actor, committer=actor)
+                To opt out from automatic synchronization from monitored upstream,
+                consult the distribution documentation or reach out to the EXD/RCM team.
+                """
+                commitmessage += '\nTriggered by ' + component + ' appearing in ' + conf['components'][component]['trigger']
+                commitmessage += '\nSource: ' + conf['components'][component]['source']
+                repo.index.commit(commitmessage, author=actor, committer=actor)
         try:
+            logging.info('Pushing changes...')
             repo.git.push()
+            logging.info('Successfully pushed.')
         except git.exc.GitCommandError:
+            logging.warning('Pushing failed!')
             pass
     except:
+        logging.warning('Failed to fetch repositories!')
         return
     try:
         shutil.rmtree(repodir)
@@ -148,7 +158,16 @@ def build_component(component):
     """
     global conf
     global status
-    pass
+    if conf['components'][component]['build']:
+        try:
+            kojiconf = koji.read_config(profile_name=conf['components'][component]['profile'])
+            session = koji.ClientSession(kojiconf['server'], opts=kojiconf)
+            session.gssapi_login()
+            session.build(conf['destination'] + conf['components'][component]['destination'], conf['components'][component]['target'])
+        except:
+            logging.warning('Build submission for component ' + component + ' failed!')
+    else:
+        logging.info(component + ' not configured for build, skipping.')
 
 def handle_message(message):
     """Handles message bus messages and triggers SCM syncs and
@@ -156,21 +175,34 @@ def handle_message(message):
     """
     global conf
     global status
+    logging.debug('Message received.  Topic: ' + message.topic)
     if message.topic.endswith('buildsys.tag'):
         component = message.body['name']
         tag = message.body['tag']
+        logging.debug('Tagging message received for ' + component + ' (tag: ' + tag + ')')
         if component in conf['components'].keys() and tag == conf['components'][component]['trigger']:
+            logging.info('Synchronizing ' + component + '...')
             merge_component(component)
             if conf['components'][component]['build']:
+                logging.info('Building ' + component + '...')
                 build_component(component)
+            else:
+                logging.info('Skipping build for ' + component + '...')
+        else:
+            logging.debug('Not configured to sync this component.  Skipping.')
 
 def main():
     global conf
     global status
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.INFO)
+    logging.info('DistroBaker starting.')
     if not conf['configuration'] or not conf['gituser'] or  not conf['gitemail']:
-        print('DISTROBAKER* variables not defined, exiting.')
+        logging.critical('DISTROBAKER* variables not defined, exiting.')
         exit()
+    logging.info('Loading configuration.')
     load_config()
+    logging.info('Configuration loaded.')
+    logging.info('Listening for messages.')
     msgapi.consume(handle_message)
 
 if __name__ == "__main__":
