0f5fa4ad56f231786b07a1a3a08bbc46a451f073
Petr Šabata <contyk@redhat.com>
2020-10-27 00:44:51 +0100

    Add merge and pull sync mechanisms
    This appears to be the cleanest way to merge upstream branches into the
    destination, removing all extra files and overwriting all content.
    
    The pull is simple.
    
    Needs error handling not to explode.
    
    Also adding a --dry-run option to the script that should be passed to
    functions to:
    
    a) not push anything to the destination SCM,
    b) not submit any builds, and
    c) not upload anything to the destination cache (verifying downloads is
    fine, I think)
    
    Also fixing a syntax error, heh.
    
    Signed-off-by: Petr Šabata <contyk@redhat.com>

--- lib/distrobaker.py
+++ lib/distrobaker.py
@@ -211,12 +211,22 @@ def sync_repo(comp, ns='rpms'):
     logging.debug('Successfully fetched upstream repository for {}/{}.'.format(ns, comp))
     if c['main']['control']['merge']:
         logging.debug('Attempting to synchronize the {}/{} branches using the merge mechanism.'.format(ns, comp))
-        # TODO: Implement this
-        pass
+        # TODO: Generate a random branch name for the temporary branch in switch
+        # TODO: Handle errors
+        actor = git.Actor(c['main']['git']['user'], c['main']['git']['email'])
+        repo.git.checkout('source/{}'.format(sscm['ref']))
+        repo.git.switch('-c', 'source')
+        repo.git.merge('--allow-unrelated-histories', '--no-commit', '-s', 'ours', dscm['ref'])
+        repo.index.commit('Temporary working tree merge', author=actor, committer=actor)
+        repo.git.checkout(dscm['ref'])
+        repo.git.merge('--no-commit', '--squash', 'source')
+        repo.index.commit(c['main']['git']['message'] + c['main']['source']['scm'] + c['comps'][ns][comp]['source'], author=actor, committer=actor)
+        logging.debug('Successfully merged {}/{} with upstream.'.format(ns, comp))
     else:
         logging.debug('Attempting to synchronize the {}/{} branches using the clean pull mechanism.'.format(ns, comp))
-        # TODO: Implement this
-        pass
+        # TODO: Handle errors
+        repo.git.pull('source', sscm['ref'])
+        logging.debug('Successfully pulled {}/{} from upstream.'.format(ns, comp))
     logging.debug('Component {}/{} successfully synchronized.'.format(ns, comp))
     if os.path.isfile(os.path.join(tempdir.name, 'sources')):
         logging.debug('Lookaside cache sources for {}/{} found, synchronizing.'.format(ns, comp))
@@ -310,7 +320,7 @@ def build_comp(comp, ref, ns='rpms'):
             return task
         except:
             pass
-    else if ns == 'modules':
+    elif ns == 'modules':
         logging.critical('Cannot build {}/{}; module building not implemented.'.format(ns, comp))
         return None
     else:
