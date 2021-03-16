ab12727ca24de373fcfddf17b5caaeca1be140ac
Merlin Mathesius <mmathesi@redhat.com>
2021-03-16 16:57:37 -0500

    Add test suite with a few initial unit tests
    Signed-off-by: Merlin Mathesius <mmathesi@redhat.com>

---
+++ tests/helpers.py
@@ -0,0 +1,83 @@
+# SPDX-License-Identifier: MIT
+
+import importlib
+import os
+import subprocess
+import tempfile
+
+GIT_HASH_REGEX = r"^[0-9a-f]{5,40}$"
+
+DATA_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data")
+
+
+def strings_with_substring(strings, substring):
+    return [string for string in strings if substring in string]
+
+
+def import_path(path):
+    """Imports python script as a module
+
+    :param path: Path to python script to import
+    :returns: imported module object
+    """
+    module_name = os.path.basename(path).replace("-", "_")
+    spec = importlib.util.spec_from_loader(
+        module_name, importlib.machinery.SourceFileLoader(module_name, path)
+    )
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    # sys.modules[module_name] = module
+    return module
+
+
+def run_cmds(cmds):
+    cwd = None
+    for cmd in cmds:
+        if cmd[0] == "cd":
+            cwd = cmd[1]
+        proc = subprocess.Popen(
+            cmd, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        out, err = proc.communicate()
+
+
+def setup_test_repo(git_repo_dir, cfg_file=None):
+    clone_dirobj = tempfile.TemporaryDirectory()
+    clone_dir = clone_dirobj.name
+
+    # setup a simple bare repo containing a README and a optional config file
+    cmds = [
+        ["cd", "/tmp"],
+        ["git", "init", "--bare", git_repo_dir],
+        ["rm", "-rf", clone_dir],
+        ["git", "clone", git_repo_dir, clone_dir],
+        ["cd", clone_dir],
+        ["git", "config", "user.name", "John Doe"],
+        ["git", "config", "user.email", "jdoe@example.com"],
+        ["bash", "-c", "echo test > README"],
+    ]
+    if cfg_file:
+        cmds.extend(
+            [
+                ["cp", cfg_file, "distrobaker.yaml"],
+            ]
+        )
+    cmds.extend(
+        [
+            ["git", "add", "."],
+            ["git", "commit", "-m", "Initial commit"],
+            ["git", "push"],
+            ["cd", git_repo_dir],
+            ["git", "branch", "-m", "main"],
+        ]
+    )
+    run_cmds(cmds)
+
+
+def last_commit(repodir):
+    cmd = ["git", "rev-parse", "--verify", "HEAD"]
+    proc = subprocess.Popen(
+        cmd, cwd=repodir, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    )
+    out, err = proc.communicate()
+    return out.rstrip()
---
+++ tests/test_config.py
@@ -0,0 +1,100 @@
+# SPDX-License-Identifier: MIT
+
+import distrobaker
+import helpers
+import os
+import pytest
+import sys
+import tempfile
+
+from parameterized import parameterized
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
+
+
+class TestConfigSetting(unittest.TestCase):
+    def test_initial_config(self):
+        # configuration should start out empty
+        cfg = distrobaker.get_config()
+        self.assertIs(type(cfg), dict)
+        self.assertFalse(cfg)
+
+    def test_load_config(self):
+        with tempfile.TemporaryDirectory() as td:
+            helpers.setup_test_repo(
+                td, os.path.join(helpers.DATA_DIR, "config", "distrobaker.yaml")
+            )
+            # attempting to load config without specifying a branch will fail
+            cfg = distrobaker.load_config(td)
+            self.assertIsNone(cfg)
+            # try again specifying the branch
+            cfg = distrobaker.load_config(td + "#main")
+
+        print("DEBUG loaded config = %s", cfg, file=sys.stderr)
+        self.assertIs(type(cfg), dict)
+        self.assertIsNotNone(cfg)
+        # make sure what was loaded matches get_config()
+        self.assertEqual(cfg, distrobaker.get_config())
+
+        # verify some derived values are present in the configuration
+        # with the expected values
+        self.assertEqual(
+            cfg["comps"]["rpms"]["ipa"],
+            {
+                "source": "freeipa.git#f33",
+                "destination": "ipa.git#fluff-42.0.0-alpha",
+                "cache": {"source": "freeipa", "destination": "ipa"},
+            },
+        )
+        self.assertEqual(
+            cfg["comps"]["modules"]["testmodule:master"],
+            {
+                "source": "testmodule.git#master",
+                "destination": "testmodule#stream-master-fluff-42.0.0-alpha-experimental",
+                "cache": {"source": "testmodule", "destination": "testmodule"},
+            },
+        )
+
+    # test for failure when loading config files that are missing required sections
+    # (this is just a randomly selected few of many possibilities)
+    @parameterized.expand(
+        [
+            # (testcase_name, config_file, expected_error)
+            (
+                "no configuration",
+                "distrobaker-no-configuration.yaml",
+                "configuration block is missing",
+            ),
+            (
+                "no trigger",
+                "distrobaker-no-trigger.yaml",
+                "trigger missing",
+            ),
+            (
+                "no source profile",
+                "distrobaker-no-source-profile.yaml",
+                "source.profile missing",
+            ),
+        ]
+    )
+    def test_load_config_missing_section(
+        self, testcase_name, config_file, expected_error
+    ):
+        with tempfile.TemporaryDirectory() as td:
+            helpers.setup_test_repo(
+                td,
+                os.path.join(helpers.DATA_DIR, "config", config_file),
+            )
+            with self.assertLogs(distrobaker.logger) as cm:
+                cfg = distrobaker.load_config(td + "#main")
+            self.assertIsNone(cfg)
+            # make sure expected_error appears in logger output
+            self.assertTrue(
+                helpers.strings_with_substring(cm.output, expected_error),
+                msg="'{}' not found in logger output: {}".format(
+                    expected_error, cm.output
+                ),
+            )
